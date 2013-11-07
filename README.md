0638fe4104ab5e62d24442192eb36b1ec6bbd314
Matt Curtis <matt.r.curtis@gmail.com>
2013-11-07 20:04:25 +1100

    updated changed docs

--- README.md
+++
@@ -1,3 +0,0 @@
-Parse strings with CSV fields into s-expressions
-
-a,b,"c,d" -> ("a" "b" "c,d")
--- parse-csv.el
+++ parse-csv.el
@@ -1,3 +1,62 @@
+;;; parse-csv.el --- Parse CSV fields
+
+;; Copyright (C) 2002-2006 Edward Marco Baringer
+;; All rights reserved.
+;;
+;; Author:	Edward Marco Baringer (Common Lisp)
+;; 		Curtis <matt.r.curtis@gmail.com> (Emacs Lisp)
+;; Maintainer: Matt Curtis <matt.r.curtis@gmail.com>
+;; Version: 0.1
+;; Keywords: csv
+;; URL: https://github.com/mrc/el-csv
+
+;; This file is not part of GNU Emacs.
+;;
+;; Redistribution and use in source and binary forms, with or without
+;; modification, are permitted provided that the following conditions are
+;; met:
+;;
+;;  - Redistributions of source code must retain the above copyright
+;;    notice, this list of conditions and the following disclaimer.
+;;
+;;  - Redistributions in binary form must reproduce the above copyright
+;;    notice, this list of conditions and the following disclaimer in the
+;;    documentation and/or other materials provided with the distribution.
+;;
+;;  - Neither the name of Edward Marco Baringer, nor BESE, nor the names
+;;    of its contributors may be used to endorse or promote products
+;;    derived from this software without specific prior written permission.
+;;
+;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+;; "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+;; LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+;; A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT
+;; OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+;; SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+;; LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+;; DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+;; THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+;; (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+;; OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+;;; Commentary:
+;;
+;; Parse strings with CSV fields into s-expressions
+;;
+;; This file implements `csv->list' and `parse-csv-string'.
+;;
+;; parse-csv-string is ported from Edward Marco Baringer's csv.lisp
+;; http://common-lisp.net/project/bese/repos/arnesi_dev/src/csv.lisp
+;; It was ported to Emacs Lisp by Matt Curtis.
+;;
+;; (csv->list "a,b,\"c,d\"")
+;;     => ("a" "b" "c,d")
+;;
+;; (parse-csv-string "a;b;'c;d'" ?\; ?\')
+;;     => ("a" "b" "c;d")
+
+;;; Code:
+
 (defun csv->list (row)
   "Parse a string containing comma-separated values into a list
 of strings, respecting double-quoted strings (which may contain
@@ -47,35 +106,6 @@ quoting character."
            (setq current-word (concat current-word (char-to-string current))))))
        (incf offset)))))
 
-;; parse-csv-string is ported from Edward Marco Baringer's csv.lisp
-;; http://common-lisp.net/project/bese/repos/arnesi_dev/src/csv.lisp
-;;
-;; Copyright (c) 2002-2006, Edward Marco Baringer
-;; All rights reserved. 
-;; 
-;; Redistribution and use in source and binary forms, with or without
-;; modification, are permitted provided that the following conditions are
-;; met:
-;; 
-;;  - Redistributions of source code must retain the above copyright
-;;    notice, this list of conditions and the following disclaimer.
-;; 
-;;  - Redistributions in binary form must reproduce the above copyright
-;;    notice, this list of conditions and the following disclaimer in the
-;;    documentation and/or other materials provided with the distribution.
-;;
-;;  - Neither the name of Edward Marco Baringer, nor BESE, nor the names
-;;    of its contributors may be used to endorse or promote products
-;;    derived from this software without specific prior written permission.
-;; 
-;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-;; "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-;; LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-;; A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT
-;; OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-;; SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-;; LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-;; DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-;; THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-;; (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-;; OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+(provide 'parse-csv)
+
+;;; parse-csv.el ends here
